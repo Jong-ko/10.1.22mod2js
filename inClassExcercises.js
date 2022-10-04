b87eb1ea7214bdc39e0847129b50096f67234556
Jeremy Ongko <ongkojeremy@gmail.com>
2022-10-04 17:34:56 -0400

    Initial Commit

---
+++ inClassExcercises.js
@@ -0,0 +1,23 @@
+function greaterInt(a,b){
+    if (a > b) {
+        console.log(a)
+    }
+    else {
+        console.log(b)
+    }
+}
+
+//greaterInt(1,2)
+
+function productOfThree(a,b,c) {
+    let product = a * b * c
+    if (product > 0) {
+        console.log('the sign is +')
+    }
+    else {
+        console.log('the sign is -')
+    }
+}
+
+//productOfThree(12, -23 ,-21)
+
